2be30d58b8dcb963bbb0ceb93c77445ada4c3d7c
rxchtan <r.tan3009@hotmail.com>
2020-07-21 22:41:03 +0800

    edited package.json

--- server.js
+++ server.js
@@ -342,7 +342,15 @@ app.get('/api/replies/:commentId', function (req, res) {
   });
 });
 
-
+/**
+ * Production Build
+ */
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './', 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
